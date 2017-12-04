116ff813ce130c8a90b2ca2ab9620c498cfad38c
Yukarin <ripple-495@outlook.com>
2017-12-03 19:02:02 -0500

    please check back again tate
    made some modifications to your code, details in comments

--- grid.cc
+++ grid.cc
@@ -6,113 +6,94 @@
 #include "cell.h"
 
 Grid::Grid(){
-
-
+    
+    
     theGrid = std::vector<std::vector<Cell>>(18);
-
+    
     for (size_t i = 0; i < 18; ++i){
         for (size_t j = 0; j < 11 ; ++j){
             theGrid[i].emplace_back(Cell(i,j));
-
+            
             //attach
-
+            
         }
     }
-
+    
     td = new TextDisplay();
     hitbox = std::vector<int> (11,17);
-
-
+    
+    
 }
 
 
 
 char Grid::getBlock(int r, int c){
-
+    
     return theGrid[r][c].getBlock();
-
-
+    
+    
 }
 
 void Grid::clear(int line){
-
+    
     for (int i = line; i > 0; --i){
-
+        
         for (int col = 0; col < 11; ++col){
             theGrid[i][col].setPiece(theGrid[i-1][col].getBlock());
-
+            
             if (!theGrid[i-1][col].Filled()){
                 theGrid[i][col].unfill();
-
+                
             }
         }
     }
-
-
+    
+    
     for(auto &cell : hitbox){
-      ++cell;
-
-
+        ++cell;
+        
+        
     }
 }
+
+// -------PLEASE CHECK AGAIN WHAT YOU DID HERE PLEASE TATE-----------
+// -----------IM SORRY BUT WE NEED TO REMOVE CONFLICTS---------------
+
 bool Grid::Check(){
-bool something_cleared = false; // use in level 4
-  for (int row = 0; row < 18; ++row){
-    bool full = false;
-    for (int col = 0; col < 11; ++ col){
-<<<<<<< HEAD
+    bool something_cleared = false; // use in level 4
+    for (int row = 0; row < 18; ++row){
+        bool full = false;
+        for (int col = 0; col < 11; ++ col){
+            if (!theGrid[row][col].Filled()){full = true;}
+        }
+        
+        if (!full) {
+            clear(row);
+            something_cleared = true;
+        }
 
-
-
-      if (!theGrid[row][col].Filled()){
-        full = true;
-      }
     }
-=======
-
->>>>>>> c09c8b0ce4f5b9511e44dda00ba69a8292878da7
-
-    if (!full) { clear(row);
-
-<<<<<<< HEAD
-      something_cleared = true;
-    }
-=======
-      if (!theGrid[row][col].Filled()){
-        full = true;
-      }
-    }
-
-    if (!full)  clear(row);
-
-
->>>>>>> c09c8b0ce4f5b9511e44dda00ba69a8292878da7
-
-
-
-  }
-return something_cleared;
+    return something_cleared; // moved outside of the loop, please revise
 }
+
 
 void Grid::setPiece(int r, int c, char piece){
-
-
+    
+    
     theGrid[r][c].setPiece(piece);
-
+    
     if(piece == ' '){
-
-          theGrid[r][c].unfill();
+        
+        theGrid[r][c].unfill();
     }
 }
 void Grid::display(){
-
+    
     std::cout << *td;
-
-
 
 }
 std::vector<int>& Grid::Hitbox(){
-
+    
     return hitbox;
 }
 
